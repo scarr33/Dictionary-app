838a4ca9959ad37f58e774d6c70dea473f427ff9
scarr33 <suchakb33@gmail.com>
2022-02-26 15:53:39 +0530

    Dictionary App

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 let input = document.querySelector("#input");
 let searchBtn = document.querySelector("#search");
+let apiKey = "5bab8143-1bf9-445d-9cb4-679ba92e6375";
+let notFound = document.querySelector(".not__found");
+let defBox = document.querySelector(".def");
+let audioBox = document.querySelector(".audio");
+let loading = document.querySelector(".loading");
 
 searchBtn.addEventListener("click", function (e) {
 	e.preventDefault();
+
+	//Clear Data
+	audioBox.innerHTML = "";
+	notFound.innerText = "";
+	defBox.innerText = "";
+
 	//Get input data
 	let word = input.value;
 	//Call API Get data
@@ -11,3 +22,58 @@ searchBtn.addEventListener("click", function (e) {
 	}
 	getData(word);
 });
+
+async function getData(word) {
+	loading.style.display = "block";
+	//Ajax call
+	const response = await fetch(
+		`https://www.dictionaryapi.com/api/v3/references/learners/json/${word}?key=${apiKey}`
+	);
+	const data = await response.json();
+
+	console.log(data);
+	//if empty result
+	if (!data.length) {
+		loading.style.display = "none";
+		notFound.innerText = "No result found";
+		return;
+	}
+
+	//If result is suggestions
+	if (typeof data[0] === "string") {
+		loading.style.display = "none";
+		let heading = document.createElement("h3");
+		heading.innerText = "Did you mean";
+		notFound.appendChild(heading);
+		data.forEach((element) => {
+			let suggestion = document.createElement("span");
+			suggestion.classList.add("suggested");
+			suggestion.innerText = element;
+			notFound.appendChild(suggestion);
+		});
+		return;
+	}
+
+	//Result found
+	loading.style.display = "none";
+	let defination = data[0].shortdef[0];
+	defBox.innerText = defination;
+
+	//Sound
+	const soundName = data[0].hwi.prs[0].sound.audio;
+	if (soundName) {
+		renderSound(soundName);
+	}
+	console.log(data);
+}
+
+function renderSound(soundName) {
+	// https://media.merriam-webster.com/soundc11
+	let subFolder = soundName.charAt(0);
+	let soundSrc = `https://media.merriam-webster.com/soundc11/${subFolder}/${soundName}.wav?key=${apiKey}`;
+
+	let aud = document.createElement("audio");
+	aud.src = soundSrc;
+	aud.controls = true;
+	audioBox.appendChild(aud);
+}
